d30bc1492349ec3ad7800e8847c15a7b52d74777
cambell-prince <cambell.prince@gmail.com>
2014-05-22 21:47:56 +1200

    Main plugin file

---
+++ ttp-wp-results.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Train To Proclaim Results
+Plugin URI: https://github.com/traintoproclaim/ttp-wp-results
+Description: Results section for Train To Proclaim
+Author: Cambell Prince
+Version: 0.1
+Author URI: http://saygoweb.com/
+*/
+
+
+?>
